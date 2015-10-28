324e2466b68ab569a2686499119c8658aca51b22
CBroskow <cbroskow@stumail.jccc.edu>
2015-10-28 05:15:07 -0500

    Tuesday night commit

--- NETWORKING LAB 03/Assets/Scripts/ServerConnection.cs
+++ NETWORKING LAB 03/Assets/Scripts/ServerConnection.cs
@@ -13,6 +13,8 @@ public class ServerConnection : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		DontDestroyOnLoad(this);
+
 		GlobalConfig globalConfig = new GlobalConfig ();
 		globalConfig.ReactorModel = ReactorModel.FixRateReactor;
 		globalConfig.ThreadAwakeTimeout = 10;
@@ -54,7 +56,63 @@ public class ServerConnection : MonoBehaviour {
 		{
 			networkEvent = NetworkTransport.Receive( out recHostId , out connectionId , out channelId , buffer , 1024 , out dataSize , out error );
 			
+			switch(networkEvent)
+			{
+				case NetworkEventType.Nothing:
+					break;
+				case NetworkEventType.ConnectEvent:
+					if( recHostId == serverSocketID )
+					{
+						Debug.Log ("Server: Player " + connectionId.ToString () + " connected!" );
+					}
+					break;
+				case NetworkEventType.DataEvent:
+					if( recHostId == serverSocketID )
+					{
+						Stream memoryStream = new MemoryStream(buffer); 
+						BinaryFormatter binaryFormatter = new BinaryFormatter();
+						string message = binaryFormatter.Deserialize( memoryStream ).ToString ();
+						Debug.Log ("Server: Received Data from " + connectionId.ToString () + "! Message: " + message );
+						RespondMessage(message, connectionId);
+					}
+					break;
+				case NetworkEventType.DisconnectEvent:
+					if( recHostId == serverSocketID )
+					{
+						Debug.Log ("Server: Received disconnect from " + connectionId.ToString () );
+					}
+					break;
+			}
+			
 		} while ( networkEvent != NetworkEventType.Nothing );
 
+	}
+
+	void SendMessage(string message, int target)
+	{
+		byte error;
+		byte[] buffer = new byte[1024];
+		Stream memoryStream = new MemoryStream(buffer);
+		BinaryFormatter binaryFormatter = new BinaryFormatter();
+		
+		binaryFormatter.Serialize (memoryStream, message);
+		NetworkTransport.Send (serverSocketID, target, reliableChannelID, buffer, (int)memoryStream.Position, out error);
+		
+		if (error != (byte)NetworkError.Ok)
+		{
+			NetworkError networkError = (NetworkError) error;
+			Debug.Log ("Error: " + networkError.ToString ());
+		}
+	}
+
+	void RespondMessage(string message, int playerID)
+	{
+		if (message == "FirstConnect")
+		{
+			Debug.Log ("message was FirstConnect! from player " + playerID.ToString());
+			SendMessage("goto_NewScene", playerID);
+			if (Application.loadedLevelName != "Scene2")
+				Application.LoadLevel("Scene2");
+		}
 	}
 }
