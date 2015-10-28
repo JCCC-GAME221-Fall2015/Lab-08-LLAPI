fd487f24978e09d52670876b80ed7c0827d1da8b
LivingValkyrie <deadwynn@gmail.com>
2015-10-27 21:32:41 -0500

    finished lab. bug fixing
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

--- lab 08 - LLAPI/Assets/ClientConnection.cs
+++ lab 08 - LLAPI/Assets/ClientConnection.cs
@@ -26,77 +26,185 @@ public class ClientConnection : MonoBehaviour {
         DontDestroyOnLoad(this);
 
         //Build the global config
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.ReactorModel = ReactorModel.FixRateReactor;
+        globalConfig.ThreadAwakeTimeout = 10;
 
         //Build the channel config
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        reliableChannelID = connectionConfig.AddChannel(QosType.ReliableSequenced);
+        unreliableChannelID = connectionConfig.AddChannel(QosType.UnreliableSequenced);
 
         //Create the host topology
+        HostTopology hostTopology = new HostTopology(connectionConfig, maxConnections);
 
         //Initialize the network transport
+        NetworkTransport.Init(globalConfig);
 
         //Open a socket for the client
+        clientSocketID = NetworkTransport.AddHost(hostTopology, 7790);
 
         //Make sure the client created the socket successfully
+        if (clientSocketID < 0) {
+            print("Server connection failed");
+        } else {
+            print("server started");
+        }
 
+        
         //Create a byte to store a possible error
+        byte error;
 
         //Connect to the server using 
         //int NetworkTransport.Connect(int socketConnectingFrom, string ipAddress, int port, 0, out byte possibleError)
         //Store the ID of the connection in clientServerConnectionID
 
+        clientServerConnectionID = NetworkTransport.Connect(clientSocketID, "127.0.0.1", 7777, 0, out error);
+
         //Display the error (if it did error out)
+        if (error != (byte) NetworkError.Ok) {
+            NetworkError networkError = (NetworkError) error;
+            print("Error: " + networkError.ToString());
+        } else {
+            isClientConnected = true;
+        }
     }
 
     void Update() {
         //If the client failed to create the socket, leave this function
+        if (!isClientConnected) {
+            print("not connected");
+            return;
+        }
 
         PollBasics();
 
         //If the user pressed the Space key
         //Send a message to the server "FirstConnect"
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("test space");
+            SendMessage("FirstConnect");
+        }
 
         //If the user pressed the R key
         //Send a message to the server "Random message!"
+        if (Input.GetKeyDown(KeyCode.R)) {
+            print("test r");
+            SendMessage("FirstConnect");
+        }
     }
 
     void SendMessage(string message) {
         //create a byte to store a possible error
+        byte error;
+
         //Create a buffer to store the message
+        byte[] buffer = new byte[1024];
+
         //Create a memory stream to send the information through
+        Stream memoryStream = new MemoryStream(buffer);
+
         //Create a binary formatter to serialize and translate the message into binary
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
         //Serialize the message
+        binaryFormatter.Serialize(memoryStream, message);
 
         //Send the message from this client, over the client server connection, using the reliable channel
+        NetworkTransport.Send(clientSocketID,
+                              clientServerConnectionID,
+                              reliableChannelID,
+                              buffer,
+                              (int) memoryStream.Position,
+                              out error);
 
         //Display the error (if it did error out)
+        if (error != (byte) NetworkError.Ok) {
+            NetworkError networkError = (NetworkError) error;
+            print("Error: " + networkError.ToString());
+        }
     }
 
     void InterperateMessage(string message) {
         //if the message is "goto_NewScene"
         //load the level named "Scene2"
+        if (message == "goto_NewScene") {
+            Application.LoadLevel("Scene2");
+        }
     }
 
     void PollBasics() {
         //prepare to receive messages by practicing good bookkeeping
 
-        //do
-        //Receive network events
-        //switch on the network event types
-        //if nothing, do nothing
-        //if connection
-        //verify that the message was meant for me
-        //debug out that i connected to the server, and display the ID of what I connected to
-        //set my bool that is keeping track if I am connected to a server to true
-        //if data event
-        //verify that the message was meant for me and if I am connected to a server
-        //decode the message (bring it through the memory stream, deseralize it, translate the binary)
-        //Debug the message and the connection that the message was sent from 
-        InterperateMessage(); //the message to interperate
+        int recClientId; //who recieved message
+        int connectionId; //who sent message
+        int channelId; //what channel message sent from
+        int dataSize; //how large message can be
+        byte[] buffer = new byte[1024]; //actual message
+        byte error; //if there is an error
 
-        //if disconnection
-        //verify that the message was meant for me, and that I am disconnecting from the current connection I have with the server
-        //debug that I disconnected
-        //set my bool that is keeping track if I am connected to a server to false
-        //while (the network event I am receiving is not Nothing)
+        NetworkEventType networkEvent = NetworkEventType.DataEvent;
+
+        //do
+        do {
+            //Receive network events
+            networkEvent = NetworkTransport.Receive(out recClientId,
+                                                    out connectionId,
+                                                    out channelId,
+                                                    buffer,
+                                                    1024,
+                                                    out dataSize,
+                                                    out error);
+
+            //switch on the network event types
+            switch (networkEvent) {
+                //if nothing, do nothing
+                //if connection
+                case NetworkEventType.ConnectEvent:
+
+                    //verify that the message was meant for me
+                    if (recClientId == clientSocketID) {
+                        //debug out that i connected to the server, and display the ID of what I connected to
+                        print("Client: connected to: " + connectionId);
+
+                        //set my bool that is keeping track if I am connected to a server to true
+                        isClientConnected = true;
+                    }
+                    break;
+
+                //if data event
+                case NetworkEventType.DataEvent:
+
+                    //verify that the message was meant for me and if I am connected to a server
+                    if (recClientId == clientSocketID) {
+                        //decode the message (bring it through the memory stream, deseralize it, translate the binary)
+                        Stream memoryStream = new MemoryStream(buffer);
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+                        string message = binaryFormatter.Deserialize(memoryStream).ToString();
+
+                        //Debug the message and the connection that the message was sent from 
+                        print("Client: recieved message " + message + " from " + connectionId);
+
+                        //the message to interperate
+                        InterperateMessage(message);
+                    }
+                    break;
+
+                //if disconnection
+                case NetworkEventType.DisconnectEvent:
+
+                    //verify that the message was meant for me, and that I am disconnecting from the current connection I have with the server
+                    if (recClientId == clientSocketID) {
+                        //debug that I disconnected
+                        print("Client: disconnected from server");
+
+                        //set my bool that is keeping track if I am connected to a server to false
+                        isClientConnected = false;
+                    }
+                    break;
+            }
+
+            //while (the network event I am receiving is not Nothing)
+        } while (networkEvent != NetworkEventType.Nothing);
     }
 }
-    
